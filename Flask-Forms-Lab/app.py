a90447bbf854b5af8cf8064042de0e827e04ebce
jewelmu24-meet <139688840+jewelmu24-meet@users.noreply.github.com>
2023-07-25 10:22:59 +0300

    flask forms lab

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -18,22 +18,40 @@ def login():
 	if request.method=='POST':
 		user= request.form['username']
 		p=request.form['password']
-		return render_template('login.html')
+
+		if user== username and p==password:
+			return redirect(url_for('home'))
+
+		else:
+			return render_template('login.html')
 
 	else:
 		return render_template('login.html')
 
 
 
-
 @app.route('/home')
 def home():
-	return render_template('home.html')
+	#return render_template('home.html')
 
-  	
-  
+ return render_template('home.html', face=facebook_friends)
 
 
+
+@app.route('/friend_exists/<string:name>')
+def friend_exists(name):
+	if name in facebook_friends:
+		friends = True
+	else:
+		friends= False
+	return render_template('friend_exists.html', friends=friends, name=name)
+
+
+
+# Dynamic Routing Example
+# @app.route('/home/<string:student>')
+# def home(student):
+# 	return render_template('home.html', name = student, face=facebook_friends)
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
